b17b9befd4e656c32ad8d7d02c40943fa9ab1116
nrw300 <njord.romijn.witsiers@gmail.com>
2023-03-20 12:01:47 +0100

    20/03/23

--- Test/javascript.js
+++ Test/javascript.js
@@ -1,22 +1,16 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
+sidebar = document.querySelector(".sidebar");
+closeBtn = document.querySelector("#btn");
 
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
   menuBtnChange();//calling the function(optional)
 });
 
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
  if(sidebar.classList.contains("open")){
-   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the icons class
  }else {
-   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the icons class
  }
 }
