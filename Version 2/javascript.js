1d0409b1fe28991c0ede863afe847f9c621146e6
nrw300 <njord.romijn.witsiers@gmail.com>
2023-04-02 22:25:20 +0200

    02/04/23

--- Version 2/javascript.js
+++ Version 2/javascript.js
@@ -4,6 +4,8 @@ closeBtn = document.querySelector("#btn");
 timeline = document.querySelector(".timeline-body");
 sortBtn = document.querySelector("#sort_btn");
 
+
+
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
   menuBtnChange();//calling the function(optional)
@@ -18,15 +20,19 @@ function menuBtnChange() {
  }
 }
 
-sortBtn.addEventListener("click", ()=>{
-  career.classList.toggle("top");
-  timelineBtnChange();
-});
+// preload images:
+var images = new Array();
 
-function timelineBtnChange() {
-  if (timeline.classList.contains("top")) {
-    sortBtn.classList.replace("bx-vertical-bottom", "bx-vertical-top");
-  } else {
-    sortBtn.classList.replace("bx-vertical-top", "bx-vertical-bottom");
-  }
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
 }
+
+preloadImages("images/logo.png", "images/IMG.png", "images/njord2.png", "images/njord.jpg");
